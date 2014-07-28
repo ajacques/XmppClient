143f9a03083c29469cf0ba7561e4de688e0a103c
Cody Martin <cody@technowizardry.net>
2014-07-28 04:29:13 +0000

    Took out unused imports and renamed package

--- SeniorProject/src/net/technowizardry/xmppclient/UI/HomeActivity.java
+++ SeniorProject/src/net/technowizardry/xmppclient/UI/HomeActivity.java
@@ -9,8 +9,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
